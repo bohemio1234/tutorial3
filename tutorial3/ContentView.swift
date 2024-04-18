23d67aa073223e2c641fc17361d154b449a5d628
Im Chanwoo <bohemio1234@naver.com>
2024-04-18 20:17:10 +0900

    Initial Commit

---
+++ tutorial3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tutorial3
+//
+//  Created by cksdn2221 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tutorial3/tutorial3App.swift
@@ -0,0 +1,17 @@
+//
+//  tutorial3App.swift
+//  tutorial3
+//
+//  Created by cksdn2221 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct tutorial3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
